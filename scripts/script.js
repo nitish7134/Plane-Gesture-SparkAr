e1d2e5d72090eff1bb784bcb2ccbfcb1fe90e7fd
Nitish Mittal <nitish7134@gmail.com>
2020-05-19 20:41:33 +0530

    efe

--- scripts/script.js
+++ scripts/script.js
@@ -8,17 +8,18 @@ const Diagnostics = require('Diagnostics');
 const sceneRoot = Scene.root;
 
 const isBackCam = Patches.outputs.getBoolean("BackCamera");
+const isFace = Patches.outputs.getBoolean("FaceTracker");
 
 var isInititalised = false;
 var isBladeRotate = false;
 
 
 Promise.all([
-    sceneRoot.findFirst('planeTracker0'),
-    sceneRoot.findFirst('InstructionText'),
-    sceneRoot.findFirst('placer')
-])
-    .then(function (objects) {
+        sceneRoot.findFirst('planeTracker0'),
+        sceneRoot.findFirst('InstructionText'),
+        sceneRoot.findFirst('placer')
+    ])
+    .then(function(objects) {
         const planeTracker = objects[0];
         const instructext = objects[1];
         const placer = objects[2];
@@ -46,32 +47,35 @@ Promise.all([
         placerTransform.scaleY = 0;
         placerTransform.scaleZ = 0;
         instructext.text = "Tap To Place";
-        TouchGestures.onTap().subscribe(function (gesture) {
+        TouchGestures.onTap().subscribe(function(gesture) {
+            Diagnostics.log('tap gesture detected');
+
             if (isBackCam) {
-                Diagnostics.log('tap gesture detected');
 
                 if (isInititalised) {
-                    instructext.text = "Tap To Place";
+                    if (!isFace)
+                        instructext.text = "Tap To Place";
                     isInititalised = false;
                     placerTransform.scaleX = 0;
                     placerTransform.scaleY = 0;
                     placerTransform.scaleZ = 0;
-                }
-                else {
-                    instructext.text = "";
-                    isInititalised = true;
-                    placerTransform.scaleX = 1;
-                    placerTransform.scaleY = 1;
-                    placerTransform.scaleZ = 1;
+                } else {
+                    if (!isFace) {
+                        instructext.text = "";
+                        isInititalised = true;
+                        placerTransform.scaleX = 1;
+                        placerTransform.scaleY = 1;
+                        placerTransform.scaleZ = 1;
+                    }
                 }
                 Patches.inputs.setBoolean("spawnLoop", isInititalised);
                 Diagnostics.log("isInititalised- " + isInititalised);
             }
         });
-        TouchGestures.onPan().subscribe(function (gesture) {
+        TouchGestures.onPan().subscribe(function(gesture) {
 
 
-            if (isBackCam && isInititalised) {
+            if (isBackCam && isInititalised && isFace) {
                 Diagnostics.log('pan gesture detected');
                 planeTracker.trackPoint(gesture.location, gesture.state);
             }
@@ -81,9 +85,9 @@ Promise.all([
             'lastScaleX': placerTransform.scaleX,
             'lastScaleY': placerTransform.scaleY,
             'lastScaleZ': placerTransform.scaleZ
-        }, function (gesture, snapshot) {
+        }, function(gesture, snapshot) {
 
-            if (isBackCam && isInititalised) {
+            if (isBackCam && isInititalised && isFace) {
                 Diagnostics.log('pinch gesture detected');
                 placerTransform.scaleX = gesture.scale.mul(snapshot.lastScaleX);
                 placerTransform.scaleY = gesture.scale.mul(snapshot.lastScaleY);
@@ -93,18 +97,18 @@ Promise.all([
 
         TouchGestures.onRotate().subscribeWithSnapshot({
             'lastRotationY': placerTransform.rotationY,
-        }, function (gesture, snapshot) {
-            if (isBackCam && isInititalised) {
+        }, function(gesture, snapshot) {
+            if (isBackCam && isInititalised && isFace) {
 
                 const correctRotation = gesture.rotation.mul(-1);
                 placerTransform.rotationY = correctRotation.add(snapshot.lastRotationY);
             }
         });
 
-        TouchGestures.onLongPress().subscribe(function (gesture) {
+        TouchGestures.onLongPress().subscribe(function(gesture) {
             Diagnostics.log('lonasfa ' + isBladeRotate + "dfdf:" + isBackCam + ":::" + isInititalised);
 
-            if (isBackCam && isInititalised) {
+            if (isBackCam && isInititalised && isFace) {
                 isBladeRotate = !isBladeRotate;
                 Diagnostics.log('long press gesture start: ' + isBladeRotate);
                 Patches.inputs.setBoolean("RotateBlades", isBladeRotate);
